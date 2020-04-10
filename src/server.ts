0571c48aa0a9fd6a76b59976b6b8349a2572f6d0
Glitch (typescript-fullstack) <none>
2020-04-10 14:12:25 +0000

    🐓🏑 Checkpoint ./src/server.ts:7701387/1141

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
 const express = require("express");
 const path = require("path");
+import * as WebSocket from 'ws';
+import * as http from 'http';
+
 const app = express();
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    ws.on('message', (message: string) => {
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    ws.send('Hi there, I am a WebSocket server');
+});
 
 app.use(express.static(path.join(__dirname, "/dist"))); // TypeScript output compiled to here.
 app.use(express.static(path.join(__dirname, "/public")));
